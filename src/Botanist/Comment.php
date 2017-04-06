6b4229c520d6b01630d78fbd78a1120691c30f5a
Alexey Puchkov <alexey@oktopost.com>
2017-04-06 05:35:07 +0300

    Add Comment and add build for travis

---
+++ src/Botanist/Comment.php
@@ -0,0 +1,38 @@
+<?php
+namespace Botanist;
+
+
+class Comment
+{
+	/**
+	 * @param mixed $source
+	 * @return string
+	 */
+	public static function get($source): string
+	{
+		if ($source instanceof \Reflector)
+		{
+			if (!method_exists($source, 'getDocComment'))
+			{
+				throw new \Exception('Unexpected object type');
+			}
+			
+			return $source->getDocComment();
+		}
+		else if (is_callable($source))
+		{
+			if (is_string($source) && function_exists($source))
+			{
+				return self::get(new \ReflectionFunction($source));
+			}
+			else
+			{
+				return self::get(new \ReflectionMethod($source));
+			}
+		}
+		else if (is_object($source))
+		{
+			return self::get(new \ReflectionClass($source));
+		}
+	}
+}
